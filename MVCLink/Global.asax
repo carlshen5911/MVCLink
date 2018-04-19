47455340544bbe6bbc5b38f9e0212c935645e9f6
carl <carl@fstop.com.tw>
2018-04-19 18:08:15 +0800

    加入專案檔案。

---
+++ MVCLink/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLink.MvcApplication" Language="C#" %>
---
+++ MVCLink/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLink.Startup))]
+namespace MVCLink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
